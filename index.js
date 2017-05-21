1fa9518d9736ffa44c7f0504f6085e2210c27fbd
Krati Ahuja <kratiahuja@gmail.com>
2017-05-20 19:53:28 -0700

    Remove proces.env.EMBER_CLI_FASTBOOT

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
   contentFor: function(type, config) {
     var content = '';
 
-    if (!process.env.EMBER_CLI_FASTBOOT && type === 'head') {
+    if (type === 'head') {
       content = '<script async type="text/javascript" src="https://calendly.com/assets/external/widget.js"></script>' +
         '<link href="https://calendly.com/assets/external/widget.css" rel="stylesheet">';
     }
