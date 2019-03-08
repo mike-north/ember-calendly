c894bde3df706472bc5c590ec282de7e1f0e4508
Ivan <kobzar.ivan1@gmail.com>
2019-03-08 14:07:19 +0300

    change widget url

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ module.exports = {
 
     if (type === 'head') {
       content =
-        '<script async type="text/javascript" src="https://calendly.com/assets/external/widget.js"></script>' +
-        '<link href="https://calendly.com/assets/external/widget.css" rel="stylesheet">';
+        '<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>';
     }
 
     return content;
